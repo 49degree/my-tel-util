734e84065840b5177570792a21c69917e1a65c16
TOMCAT.YANG@gmail.com <TOMCAT.YANG@gmail.com@88241bef-143b-a1db-c22c-68f70da16bca>
2014-05-26 14:36:40 +0000

    人流统计

--- StoreMonitor/src/com/skyeyes/storemonitor/Fragment/TrafficStatisticsDayFrg.java
+++ StoreMonitor/src/com/skyeyes/storemonitor/Fragment/TrafficStatisticsDayFrg.java
@@ -1,20 +1,23 @@
 package com.skyeyes.storemonitor.Fragment;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import org.achartengine.GraphicalView;
 import org.achartengine.chart.LineChart;
 import org.achartengine.chart.XYChart;
-import org.achartengine.renderer.XYMultipleSeriesRenderer;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -24,28 +27,29 @@ import com.skyeyes.base.cmd.bean.impl.SendObjectParams;
 import com.skyeyes.base.cmd.bean.impl.manucount.ReceiveAvgHourManuByDay;
 import com.skyeyes.base.exception.CommandParseException;
 import com.skyeyes.base.util.DateUtil;
+import com.skyeyes.base.util.Log;
 import com.skyeyes.storemonitor.R;
 import com.skyeyes.storemonitor.process.impl.CountManuCmdProcess;
 import com.skyeyes.storemonitor.service.DevicesService;
 
 @SuppressLint("NewApi")
-public class TrafficStatisticsDayFrg extends SuperFragment {
+public class TrafficStatisticsDayFrg extends SuperFragment implements
+		OnClickListener {
 	private View mItemView;
 	private TextView averageTime;
 	private TextView total;
-	
 	private LinearLayout layout;
-	
-	
-	
 	private GraphicalView mView;
-	
+	private Button timeBtn;
+	private Button lastDayIbtn;
+	private Button nextDayIbtn;
 	private XYChart chart;
-	
+	public Calendar mCurCalendar = Calendar.getInstance(); // 当前的时间
+
 	private final int MONTH_VIEW_REFLESH = 1;
 	// 测试数据
 	private ArrayList<CountManuResultBean> monthResultBeans;
-	Handler handler = new Handler(){
+	Handler handler = new Handler() {
 		@Override
 		public void handleMessage(Message msg) {
 			// TODO Auto-generated method stub
@@ -53,11 +57,14 @@ public class TrafficStatisticsDayFrg extends SuperFragment {
 			case MONTH_VIEW_REFLESH:
 				layout.removeAllViews();
 				xyMultipleSeriesRenderer.setYAxisMax(20.0);
-				xyMultipleSeriesRenderer.setPanLimits(new double[] { -1, monthResultBeans.size(), 0, 80 }); // 限制xy轴的长度
+				xyMultipleSeriesRenderer.setPanLimits(new double[] { -1,
+						monthResultBeans.size(), 0, 80 }); // 限制xy轴的长度
 
-				chart = new LineChart(getDemoDataset(monthResultBeans), xyMultipleSeriesRenderer);
+				chart = new LineChart(getDemoDataset(monthResultBeans),
+						xyMultipleSeriesRenderer);
 				mView = new GraphicalView(getActivity(), chart);
-				averageTime.setText(String.valueOf(monthResultBeans.get(0).avgTime));
+				averageTime
+						.setText(String.valueOf(monthResultBeans.get(0).avgTime));
 				total.setText(String.valueOf(monthResultBeans.get(0).inManu));
 				layout.addView(mView);
 				break;
@@ -65,9 +72,10 @@ public class TrafficStatisticsDayFrg extends SuperFragment {
 			default:
 				break;
 			}
-			
+
 		}
 	};
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -75,19 +83,29 @@ public class TrafficStatisticsDayFrg extends SuperFragment {
 		super.onCreateView(inflater, container, savedInstanceState);
 		mItemView = inflater.inflate(R.layout.traffic_statistivs_day_frg_view,
 				container, false);
-		layout = (LinearLayout)mItemView.findViewById(R.id.my_chart);
-		total = (TextView)mItemView.findViewById(R.id.total_values);
-		averageTime = (TextView)mItemView.findViewById(R.id.average_time_value);
+		layout = (LinearLayout) mItemView.findViewById(R.id.my_chart);
+		total = (TextView) mItemView.findViewById(R.id.total_values);
+		averageTime = (TextView) mItemView
+				.findViewById(R.id.average_time_value);
+		lastDayIbtn = (Button) mItemView.findViewById(R.id.last_day_ibtn);
+		nextDayIbtn = (Button) mItemView.findViewById(R.id.next_day_ibtn);
+		timeBtn = (Button) mItemView.findViewById(R.id.search_date_bt);
+		timeBtn.setText(DateUtil
+				.getTimeStringFormat(mCurCalendar, "yyyy-MM-dd"));
 
+		lastDayIbtn.setOnClickListener(this);
+		nextDayIbtn.setOnClickListener(this);
+		timeBtn.setOnClickListener(this);
 		return mItemView;
 	}
-	
+
 	@Override
 	public void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
 		getManucountByDay("2014-05-17 00:00:00");
 	}
+
 	/**
 	 * 按日统计人流
 	 * 
@@ -111,6 +129,7 @@ public class TrafficStatisticsDayFrg extends SuperFragment {
 		}
 
 	}
+
 	/**
 	 * 按日统计人流
 	 * 
@@ -127,24 +146,68 @@ public class TrafficStatisticsDayFrg extends SuperFragment {
 
 		public void onProcess(ReceiveAvgHourManuByDay receiveCmdBean) {
 			super.onProcess(receiveCmdBean);
-			for (CountManuResultBean countManuResultBean : receiveCmdBean.countManuResultBeans) {
-				Log.e("chenlong",
-						DateUtil.getTimeStringFormat(countManuResultBean.time,
-								DateUtil.TIME_FORMAT_YMDHMS)
-								+ ":"
-								+ countManuResultBean.dayofWeet
-								+ ":"
-								+ countManuResultBean.inManu
-								+ ":"
-								+ countManuResultBean.outManu
-								+ ":"
-								+ countManuResultBean.avgTime);
-			}
-			monthResultBeans =  receiveCmdBean.countManuResultBeans;
+			// for (CountManuResultBean countManuResultBean :
+			// receiveCmdBean.countManuResultBeans) {
+			// Log.e("chenlong",
+			// DateUtil.getTimeStringFormat(countManuResultBean.time,
+			// DateUtil.TIME_FORMAT_YMDHMS)
+			// + ":"
+			// + countManuResultBean.dayofWeet
+			// + ":"
+			// + countManuResultBean.inManu
+			// + ":"
+			// + countManuResultBean.outManu
+			// + ":"
+			// + countManuResultBean.avgTime);
+			// }
+			monthResultBeans = receiveCmdBean.countManuResultBeans;
 			Message message = new Message();
 			message.what = MONTH_VIEW_REFLESH;
 			handler.sendMessage(message);
 		}
 
 	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.last_day_ibtn:
+			mCurCalendar = DateUtil.getCalendarByNumDay(mCurCalendar, -1);
+			timeBtn.setText(DateUtil.getTimeStringFormat(mCurCalendar,
+					"yyyy-MM-dd"));
+			getManucountByDay(DateUtil.getTimeStringFormat(mCurCalendar,
+					"yyyy-MM-dd HH:mm:ss"));
+
+			break;
+		case R.id.next_day_ibtn:
+			mCurCalendar = DateUtil.getCalendarByNumDay(mCurCalendar, 1);
+			timeBtn.setText(DateUtil.getTimeStringFormat(mCurCalendar,
+					"yyyy-MM-dd"));
+			getManucountByDay(DateUtil.getTimeStringFormat(mCurCalendar,
+					"yyyy-MM-dd HH:mm:ss"));
+			break;
+		case R.id.search_date_bt:
+//			new DatePickerDialog(getActivity(), listener,
+//					mCurCalendar.get(Calendar.YEAR),
+//					mCurCalendar.get(Calendar.MONTH),
+//					mCurCalendar.get(Calendar.DAY_OF_MONTH)).show();
+			break;
+		default:
+			break;
+		}
+
+	}
+
+//	private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() { //
+//
+//		@Override
+//		public void onDateSet(DatePicker arg0, int y, int m, int d) {
+//			Log.i("chenlong", "time "+y+"-"+m+"-"+d);
+//			timeBtn.setText(y+"-"+m+"-"+d);
+//		}
+//
+//	};
+//	
+
 }
--- StoreMonitor/src/com/skyeyes/storemonitor/Fragment/TrafficStatisticsMonthFrg.java
+++ StoreMonitor/src/com/skyeyes/storemonitor/Fragment/TrafficStatisticsMonthFrg.java
@@ -1,19 +1,24 @@
 package com.skyeyes.storemonitor.Fragment;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 import org.achartengine.GraphicalView;
 import org.achartengine.chart.LineChart;
 import org.achartengine.chart.XYChart;
-import org.achartengine.renderer.XYMultipleSeriesRenderer;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -22,29 +27,34 @@ import com.skyeyes.base.cmd.bean.impl.ReceiveCountManu.CountManuResultBean;
 import com.skyeyes.base.cmd.bean.impl.SendObjectParams;
 import com.skyeyes.base.cmd.bean.impl.manucount.ReceiveAvgDayManuByMouse;
 import com.skyeyes.base.exception.CommandParseException;
+import com.skyeyes.base.util.DateUtil;
 import com.skyeyes.storemonitor.R;
 import com.skyeyes.storemonitor.process.impl.CountManuCmdProcess;
 import com.skyeyes.storemonitor.service.DevicesService;
 
 @SuppressLint("NewApi")
-public class TrafficStatisticsMonthFrg extends SuperFragment{
+public class TrafficStatisticsMonthFrg extends SuperFragment implements
+		OnClickListener {
 
 	private View mItemView;
 	private TextView averageTime;
 	private TextView total;
-	
+
 	private LinearLayout layout;
-	
-	
-	
+
+	private Button timeBtn;
+	private Button lastDayIbtn;
+	private Button nextDayIbtn;
+	public Calendar mCurCalendar = Calendar.getInstance(); // 当前的时间
+
 	private GraphicalView mView;
-	
+
 	private XYChart chart;
-	
+
 	private final int MONTH_VIEW_REFLESH = 1;
 	// 测试数据
 	private ArrayList<CountManuResultBean> monthResultBeans;
-	Handler handler = new Handler(){
+	Handler handler = new Handler() {
 		@Override
 		public void handleMessage(Message msg) {
 			// TODO Auto-generated method stub
@@ -52,11 +62,14 @@ public class TrafficStatisticsMonthFrg extends SuperFragment{
 			case MONTH_VIEW_REFLESH:
 				layout.removeAllViews();
 				xyMultipleSeriesRenderer.setYAxisMax(80.0);
-				xyMultipleSeriesRenderer.setPanLimits(new double[] { -1, monthResultBeans.size(), 0, 80 }); // 限制xy轴的长度
+				xyMultipleSeriesRenderer.setPanLimits(new double[] { -1,
+						monthResultBeans.size(), 0, 80 }); // 限制xy轴的长度
 
-				chart = new LineChart(getDemoDataset(monthResultBeans), xyMultipleSeriesRenderer);
+				chart = new LineChart(getDemoDataset(monthResultBeans),
+						xyMultipleSeriesRenderer);
 				mView = new GraphicalView(getActivity(), chart);
-				averageTime.setText(String.valueOf(monthResultBeans.get(0).avgTime));
+				averageTime
+						.setText(String.valueOf(monthResultBeans.get(0).avgTime));
 				total.setText(String.valueOf(monthResultBeans.get(0).inManu));
 				layout.addView(mView);
 				break;
@@ -64,29 +77,38 @@ public class TrafficStatisticsMonthFrg extends SuperFragment{
 			default:
 				break;
 			}
-			
+
 		}
 	};
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreateView(inflater, container, savedInstanceState);
-		mItemView = inflater.inflate(R.layout.traffic_statistivs_month_frg_view,
-				container, false);
-		layout = (LinearLayout)mItemView.findViewById(R.id.my_chart);
-		total = (TextView)mItemView.findViewById(R.id.total_values);
-		averageTime = (TextView)mItemView.findViewById(R.id.average_time_value);
+		mItemView = inflater.inflate(
+				R.layout.traffic_statistivs_month_frg_view, container, false);
+		layout = (LinearLayout) mItemView.findViewById(R.id.my_chart);
+		total = (TextView) mItemView.findViewById(R.id.total_values);
+		averageTime = (TextView) mItemView
+				.findViewById(R.id.average_time_value);
+		lastDayIbtn = (Button) mItemView.findViewById(R.id.last_day_ibtn);
+		nextDayIbtn = (Button) mItemView.findViewById(R.id.next_day_ibtn);
+		timeBtn = (Button) mItemView.findViewById(R.id.search_date_bt);
+		timeBtn.setText(getMonthDate(mCurCalendar,0));
 
+		lastDayIbtn.setOnClickListener(this);
+		nextDayIbtn.setOnClickListener(this);
 		return mItemView;
 	}
-	
+
 	@Override
 	public void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
 		getManucountByMonth("2014-05-01 00:00:00");
 	}
+
 	private void getManucountByMonth(String dayTime) {
 		SendObjectParams sendObjectParams = new SendObjectParams();
 
@@ -123,24 +145,62 @@ public class TrafficStatisticsMonthFrg extends SuperFragment{
 		public void onProcess(ReceiveAvgDayManuByMouse receiveCmdBean) {
 			super.onProcess(receiveCmdBean);
 			// receiveCmdBean.countManuResultBeans 这里是数据列表
-//			for (CountManuResultBean countManuResultBean : receiveCmdBean.countManuResultBeans) {
-//				Log.e("chenlong",
-//						DateUtil.getTimeStringFormat(countManuResultBean.time,
-//								DateUtil.TIME_FORMAT_YMDHMS)
-//								+ ":"
-//								+ countManuResultBean.dayofWeet
-//								+ ":"
-//								+ countManuResultBean.inManu
-//								+ ":"
-//								+ countManuResultBean.outManu
-//								+ ":"
-//								+ countManuResultBean.avgTime);
-//			}
-			monthResultBeans =  receiveCmdBean.countManuResultBeans;
+			// for (CountManuResultBean countManuResultBean :
+			// receiveCmdBean.countManuResultBeans) {
+			// Log.e("chenlong",
+			// DateUtil.getTimeStringFormat(countManuResultBean.time,
+			// DateUtil.TIME_FORMAT_YMDHMS)
+			// + ":"
+			// + countManuResultBean.dayofWeet
+			// + ":"
+			// + countManuResultBean.inManu
+			// + ":"
+			// + countManuResultBean.outManu
+			// + ":"
+			// + countManuResultBean.avgTime);
+			// }
+			monthResultBeans = receiveCmdBean.countManuResultBeans;
 			Message message = new Message();
 			message.what = MONTH_VIEW_REFLESH;
 			handler.sendMessage(message);
 		}
 	}
 
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.last_day_ibtn:
+			StringBuffer buffer =new StringBuffer();
+			buffer.append(getMonthDate(mCurCalendar,-1));
+			timeBtn.setText(buffer.toString());
+			
+			buffer.append("-01 00:00:00");
+			Log.e("chenlong", "buffer::: "+buffer.toString());
+			getManucountByMonth(buffer.toString());
+			buffer =null;
+			break;
+		case R.id.next_day_ibtn:
+			
+			StringBuffer buffer1 =new StringBuffer();
+			buffer1.append(getMonthDate(mCurCalendar,1));
+			timeBtn.setText(buffer1.toString());
+			
+			buffer1.append("-01 00:00:00");
+			Log.e("chenlong", "buffer1::: "+buffer1.toString());
+
+			getManucountByMonth(buffer1.toString());
+			buffer1 = null;
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	private String getMonthDate(Calendar cc, int month) {
+		cc.add(Calendar.MONTH, month);
+		 return new SimpleDateFormat("yyyy-MM").format(cc.getTime());
+	}
+	
 }
